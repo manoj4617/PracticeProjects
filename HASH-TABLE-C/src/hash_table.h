64b021dbcfb734d5c8ac5925109bc5c7a25bdfbf
manoj4617 <manojsainikwod4617@gmail.com>
2023-02-28 17:21:43 +0530

    added resizing feature

--- HASH-TABLE-C/src/hash_table.h
+++ HASH-TABLE-C/src/hash_table.h
@@ -1,10 +1,12 @@
-
-
-
 /*
 This is the basic structure of the hash table
 key and value, defined in a struct;
 */
+
+#define HT_INITIAL_BASE_SIZE 50
+
+static h_item HT_DELETED_ITEM = {NULL, NULL};
+
 typedef struct {
     char* key;
     char* value;
@@ -17,10 +19,12 @@ has to be tracked which is taken care by the below struct;
 typedef struct {
     int count;
     int size;
+    int base_size;
     h_item** items;
 }h_hash_table;
 
 
-void ht_delete_hash_table(h_hash_table*);
-
-
+void ht_delete_hash_table(h_hash_table* ht);
+void ht_insert(h_hash_table* ht, const char* key, const char* value);
+char* ht_search(h_hash_table* ht, const char* key);
+void ht_delete(h_hash_table* ht, const char* key);
